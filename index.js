a1903fa538431c9de85c5ff85eb40d9ab515ded4
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-22 12:28:54 -0800

    commit tree 69b8431d5f487baad969674bc2e18a39d9259b3d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else if (blocks < 42) {
+    return 42 - blocks;
+  } else {
+    return 0;
+  }
+}
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    return (start - end) * 264;
+  } else if (start < end) {
+    return (end - start) * 264;
+  } else {
+    return 0;
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  const feet = distanceTravelledInFeet(start, end);
+  if (feet <= 400) {
+    return 0;
+  } else if (feet < 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
